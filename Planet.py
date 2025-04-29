82bffc98eb63cfa9f8f5875584d18ef25ccecde8
CSAADZIDI <zidisaad.chaima@gmail.com>
2025-04-29 12:09:48 +0200

    Here is the class Planet

---
+++ Planet.py
@@ -0,0 +1,6 @@
+class Planet:
+    def __init__(self, height:int, width:int, num_fish:int, num_shark:int):
+        self.height = height
+        self.width = width
+        self.num_fish = num_fish
+        self.num_shark = num_shark
