e866c20ac916319aa31f5c20e65d9d9a8343ab57
Aruide <ruide.aurelien@gmail.com>
2025-04-29 16:43:30 +0200

    modify: modif retour de valeur de move

--- classes/fish.py
+++ classes/fish.py
@@ -1,31 +1,60 @@
-from settings import fish_reproduction_time
+from settings import fish_reproduction_time, grid_width , grid_height
 import random
 
 class Fish:
-    def __init__(self, position :[]):
-        self.position = position
+    def __init__(self, x :int, y:int):
+        self.x = x
+        self.y = y
         self.reproduction_time = fish_reproduction_time
+        self.reproduction_left =  self.reproduction_time
 
     def move(self, list):
+        old_position = {"x": self.x, "y": self.y}
         verif = {"N": list[0] , "S": list[1], "E": list[2], "W": list[3]}
         direction = []
         for key, value in verif.items():
             if(value == None):
                 direction.append(key)               
         
+        if( len(direction) > 0):
+            match(random.choice(direction)):
+                case "N":
+                    if(self.y == 0):
+                        self.y = grid_height - 1
+                    else:
+                        self.y = self.y - 1
+                case "S":
+                    #verif en bas du tableau
+                    if(grid_height - 1 == self.y):
+                        self.y = 0
+                    else:
+                        self.y = self.y + 1
+                case "W":
+                    if(self.x == 0):
+                        self.x = grid_width - 1
+                    else:
+                        self.x = self.x - 1
+                case "E":
+                    #verif à droite du tableau
+                    if(grid_width - 1 == self.x):
+                        self.x = 0
+                    else :
+                        self.x = self.x + 1
+        
+        list_result = [{"x": self.x, "y": self.y}]
 
-        match(random.choice(direction)):
-            case "N":
-                self.position = [self.position[0] + 1, self.position[1]]
-            case "S":
-                self.position = [self.position[0] - 1, self.position[1]]
-            case "E":
-                self.position = [self.position[0], self.position[1] + 1]
-            case "W":
-                self.position = [self.position[0], self.position[1] - 1]
+        if(self.reproduce(list_result[0], old_position) == True):
+            list_result.append(old_position)
 
-        return print(self.position)
+        return list_result
     
 
-    def reproduce(self):
-        return []
+    def reproduce(self, new_position, old_position):
+        if(self.reproduction_left > 0):
+            self.reproduction_left -= 1
+
+        if((new_position != old_position) and self.reproduction_left == 0):
+            self.reproduction_left = self.reproduction_time
+            return True
+        else:
+            return False
