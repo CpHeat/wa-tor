508862776bb85fb2c10199e73e6187124260a4e5
CSAADZIDI <zidisaad.chaima@gmail.com>
2025-05-07 10:40:35 +0200

    generating class diagram

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from classes.interface import Interface
 from services.persistence_handler import PersistenceHandler
 
+
+
+"""this is main module where we call all the functions to do
+"""
 interface = Interface()
 
 if __name__ == "__main__":
